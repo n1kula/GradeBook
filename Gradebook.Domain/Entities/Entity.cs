a390b5aba3125ac271a4299a44b04e8112f02692
Tomasz <n1kula@wp.pl>
2024-02-05 13:36:09 +0100

    First Entiteis added

---
+++ Gradebook.Domain/Entities/Entity.cs
@@ -0,0 +1,9 @@
+﻿namespace Gradebook.Domain.Entities
+{
+    public abstract class Entity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
---
+++ Gradebook.Domain/Entities/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace Gradebook.Domain.Entities
+{
+    public class Student : Entity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateOnly DOB { get; set; }
+        public int YearEnrolled { get; set; }
+    }
+}
