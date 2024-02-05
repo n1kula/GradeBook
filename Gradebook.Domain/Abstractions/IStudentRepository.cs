644939b85bc12f022f6f3fb0c48043f955642eea
Tomasz <n1kula@wp.pl>
2024-02-05 13:44:38 +0100

    Repository added

---
+++ Gradebook.Domain/Abstractions/IStudentRepository.cs
@@ -0,0 +1,16 @@
+﻿using Gradebook.Domain.Entities;
+
+namespace Gradebook.Domain.Abstractions
+{
+    public interface IStudentRepository
+    {
+        Task<IEnumerable<Student>> GetAllAsync(CancellationToken cancellation = default);
+        Task<Student> GetByIdAsync(int id, CancellationToken cancellation = default);
+        Task<Student> GetByEmailAsync(string email, CancellationToken cancellation = default);
+        Task<bool> IsAlreadyExistAsync(string email, CancellationToken cancellation = default);
+
+        void Add(Student student);
+        void Update(Student student);
+        void Delete(Student student);
+    }
+}
---
+++ Gradebook.Infrastructure/Repositories/StudentRepository.cs
@@ -0,0 +1,43 @@
+﻿using Gradebook.Domain.Abstractions;
+using Gradebook.Domain.Entities;
+
+namespace Gradebook.Infrastructure.Repositories
+{
+    internal class StudentRepository : IStudentRepository
+    {
+        public Task<IEnumerable<Student>> GetAllAsync(CancellationToken cancellation = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Student> GetByEmailAsync(string email, CancellationToken cancellation = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Student> GetByIdAsync(int id, CancellationToken cancellation = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsAlreadyExistAsync(string email, CancellationToken cancellation = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(Student student)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Student student)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Student student)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
