a2efc42269e00bcb14d52eb81371122731d12dab
Tomasz <n1kula@wp.pl>
2024-02-06 08:55:56 +0100

    Repository implementation

---
+++ Gradebook.Infrastructure/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using Gradebook.Domain.Abstractions;
+using Gradebook.Infrastructure.Context;
+using Gradebook.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Gradebook.Infrastructure
+{
+    public static class Extensions
+    {
+        public static object AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<IStudentRepository, StudentRepository>();
+            services.AddDbContext<GradebookDbContext>(ctx => ctx.UseSqlServer(configuration.GetConnectionString("GradebookCS")));
+            return services;
+        }
+    }
+}
--- Gradebook.Infrastructure/Repositories/StudentRepository.cs
+++ Gradebook.Infrastructure/Repositories/StudentRepository.cs
@@ -1,43 +1,51 @@
 ﻿using Gradebook.Domain.Abstractions;
 using Gradebook.Domain.Entities;
+using Gradebook.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace Gradebook.Infrastructure.Repositories
 {
     internal class StudentRepository : IStudentRepository
     {
-        public Task<IEnumerable<Student>> GetAllAsync(CancellationToken cancellation = default)
+        private readonly GradebookDbContext _dbContext;
+        public StudentRepository(GradebookDbContext dbContext)
         {
-            throw new NotImplementedException();
+           _dbContext = dbContext;   
         }
 
-        public Task<Student> GetByEmailAsync(string email, CancellationToken cancellation = default)
+        public async Task<IEnumerable<Student>> GetAllAsync(CancellationToken cancellation = default)
         {
-            throw new NotImplementedException();
+            return await _dbContext.Students.ToListAsync(cancellation);
         }
 
-        public Task<Student> GetByIdAsync(int id, CancellationToken cancellation = default)
+        public async Task<Student> GetByEmailAsync(string email, CancellationToken cancellation = default)
         {
-            throw new NotImplementedException();
+            return await _dbContext.Students.SingleOrDefaultAsync(x=>x.Email == email, cancellation);
         }
 
-        public Task<bool> IsAlreadyExistAsync(string email, CancellationToken cancellation = default)
+        public async Task<Student> GetByIdAsync(int id, CancellationToken cancellation = default)
         {
-            throw new NotImplementedException();
+            return await _dbContext.Students.SingleOrDefaultAsync(x => x.Id == id, cancellation);
+        }
+
+        public async Task<bool> IsAlreadyExistAsync(string email, CancellationToken cancellation = default)
+        {
+            return await _dbContext.Students.AnyAsync(x => x.Email == email, cancellation);
         }
 
         public void Add(Student student)
         {
-            throw new NotImplementedException();
+            _dbContext.Students.Add(student);
         }
 
         public void Update(Student student)
         {
-            throw new NotImplementedException();
+            _dbContext.Students.Update(student);
         }
 
         public void Delete(Student student)
         {
-            throw new NotImplementedException();
+            _dbContext.Students.Remove(student);
         }
     }
 }
