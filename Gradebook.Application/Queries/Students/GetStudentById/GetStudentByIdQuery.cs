f3753e6bba0372f2193c62f99a4706c2058df7c1
Tomasz <n1kula@wp.pl>
2024-02-06 23:44:04 +0100

    getStudentById+GetStudentByImal implemented

---
+++ Gradebook.Application/Queries/Students/GetStudentById/GetStudentByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using Gradebook.Application.Dtos;
+using MediatR;
+
+namespace Gradebook.Application.Queries.Students.GetStudentById
+{
+    public record GetStudentByEmailQuery(int Id) : IRequest<StudentDto>
+    {
+    }
+}
