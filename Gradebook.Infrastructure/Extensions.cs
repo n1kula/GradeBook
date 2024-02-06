62f3c6e7d3b963126dbb09cc5780cb5a48e4295d
Tomasz <n1kula@wp.pl>
2024-02-06 09:04:37 +0100

    UnitOfWork implementation

--- Gradebook.Infrastructure/Extensions.cs
+++ Gradebook.Infrastructure/Extensions.cs
@@ -11,6 +11,7 @@ namespace Gradebook.Infrastructure
     {
         public static object AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
             services.AddScoped<IStudentRepository, StudentRepository>();
             services.AddDbContext<GradebookDbContext>(ctx => ctx.UseSqlServer(configuration.GetConnectionString("GradebookCS")));
             return services;
--- Gradebook.Infrastructure/UnitOfWork.cs
+++ Gradebook.Infrastructure/UnitOfWork.cs
@@ -1,17 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Gradebook.Domain.Abstractions;
+﻿using Gradebook.Domain.Abstractions;
+using Gradebook.Domain.Entities;
+using Gradebook.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace Gradebook.Infrastructure
 {
-    public class UnitOfWork : IUnitOfWork
+    internal class UnitOfWork : IUnitOfWork
     {
-        public Task SaveChangesAsync(CancellationToken cancellationToken = default)
+        private readonly GradebookDbContext _dbContext;
+        public UnitOfWork(GradebookDbContext dbContext)
         {
-            throw new NotImplementedException();
+            _dbContext = dbContext;
+        }
+        public async Task SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            UpdateAuditableEntities();
+            await _dbContext.SaveChangesAsync(cancellationToken);
+        }
+
+        private void UpdateAuditableEntities()
+        {
+            var entires = _dbContext
+                .ChangeTracker
+                .Entries<Entity>();
+            foreach (var entry in entires)
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Entity.CreatedAt = entry.Entity.UpdatedAt = DateTime.Now;
+                }
+                if (entry.State == EntityState.Modified)
+                {
+                    entry.Entity.UpdatedAt = DateTime.Now;
+                }
+            }
         }
     }
 }
