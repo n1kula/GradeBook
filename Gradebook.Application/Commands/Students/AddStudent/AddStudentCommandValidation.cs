33490f6bd8be5f305a357b2a463838d1d0056f15
Tomasz <n1kula@wp.pl>
2024-02-07 22:33:11 +0100

    fluent validation for addStudent implemented

---
+++ Gradebook.Application/Commands/Students/AddStudent/AddStudentCommandValidation.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+
+namespace Gradebook.Application.Commands.Students.AddStudent
+{
+    internal class AddStudentCommandValidation : AbstractValidator<AddStudentCommand>
+    {
+        public AddStudentCommandValidation()
+        {
+            RuleFor(x => x.FirstName)
+                .NotEmpty().WithMessage("First nama is required")
+                .MaximumLength(50).WithMessage("First name can't be longer than 50 characters.");
+
+            RuleFor(x => x.LastName)
+                .NotEmpty().WithMessage("Last name is required")
+                .MaximumLength(50).WithMessage("LAst name can't be longer than 50 characters.");
+
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Email nama is required")
+                .MaximumLength(100).WithMessage("Email can't be longer than 50 characters.")
+                .EmailAddress().WithMessage("Invalid email format");
+
+            RuleFor(x => x.DOB)
+               .NotEmpty().WithMessage("DOB is required");
+
+            RuleFor(x => x.YearEnrolled)
+               .NotEmpty().WithMessage("Year enrolled is required");
+        }
+    }
+}
